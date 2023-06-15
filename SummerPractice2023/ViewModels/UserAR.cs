21805c50bf4436cd8229122311766ccc210d21e1
MrRamis <99258165+MrRamis@users.noreply.github.com>
2023-06-15 15:32:08 +0300

    view

--- SummerPractice2023/ViewModels/UserAR.cs
+++ SummerPractice2023/ViewModels/UserAR.cs
@@ -9,6 +9,114 @@ namespace SummerPractice2023.ViewModels
 {
     internal class UserAR : INotifyPropertyChanged
     {
+        #region
+        private string _IdUser;
+        public string IdUser
+        {
+            get
+            {
+                return _IdUser;
+            }
+            set
+            {
+                _IdUser = value;
+            }
+        }
+
+        private string _Lodin;
+        public string Lodin
+        {
+            get
+            {
+                return _Lodin;
+            }
+            set
+            {
+                _Lodin = value;
+            }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get
+            {
+                return _Password;
+            }
+            set
+            {
+                _Password = value;
+            }
+        }
+
+        private string _Image;
+        public string Image
+        {
+            get
+            {
+                return _Image;
+            }
+            set
+            {
+                _Image = value;
+            }
+        }
+
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+
+        private string _Lastname;
+        public string Lastname
+        {
+            get
+            {
+                return _Lastname;
+            }
+            set
+            {
+                _Lastname = value;
+            }
+        }
+
+        private string _Surname;
+        public string Surname
+        {
+            get
+            {
+                return _Surname;
+            }
+            set
+            {
+                _Surname = value;
+            }
+        }
+
+        private string _Status;
+        public string Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                _Status = value;
+            }
+        }
+        #endregion
+
+
+
 
 
 
