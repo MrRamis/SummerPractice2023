693e93d55ae4ee1fed855fbf3a1b8d613bdee799
MrRamis <99258165+MrRamis@users.noreply.github.com>
2023-06-15 15:21:19 +0300

    lodin

---
+++ SummerPractice2023/Models/Hash.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SummerPractice2023.Models
+{
+    internal class Hash
+    {
+        public static string? hashPassword(string password)
+        {
+            MD5 mD5 = MD5.Create();
+
+            byte[] b = Encoding.ASCII.GetBytes(password);
+            byte[] hash = mD5.ComputeHash(b);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b2 in hash)
+                sb.Append(b2.ToString("X2"));
+            return Convert.ToString(sb);
+        }
+    }
+}
---
+++ SummerPractice2023/ViewModels/UserAR.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerPractice2023.ViewModels
+{
+    internal class UserAR : INotifyPropertyChanged
+    {
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
