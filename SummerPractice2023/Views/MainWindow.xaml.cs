fe4a442f42bd3b4df8f1fc5989a5904065bb2f3e
MrRamis <99258165+MrRamis@users.noreply.github.com>
2023-06-17 21:17:12 +0300

    c

--- SummerPractice2023/Views/MainWindow.xaml.cs
+++ SummerPractice2023/Views/MainWindow.xaml.cs
@@ -1,24 +1,8 @@
-﻿using SummerPractice2023.Models;
-using SummerPractice2023.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SummerPractice2023.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SummerPractice2023.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
